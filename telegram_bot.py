777e8d2616d51ab800cfaffb8dc22d5f78016ffc
Oleksandr <oleksandrr1996@protonmail.com>
2023-02-04 15:21:00 +0200

    Created password validation function

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,10 +4,22 @@
 
 import secrets
 
+import re
+
 def gen_pass(chars, length):
     '''Return randomly generated string
        of certain length
     '''
     chars_tup = (secrets.choice(chars) for _ in range(length))
     return "".join(chars_tup)
+
+
+def is_password_valid(string_pass):
+    '''Verify that password contains
+        at leats one uppercase letter,
+        lowercase letter, number, and
+        a special symbold
+    '''
+    reg_ex = r'^(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])'
+    return re.search(pattern=reg_ex, string=string_pass) is not None
 
